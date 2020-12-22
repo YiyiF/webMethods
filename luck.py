9499879f844ac3569a26c5edb303527e89012559
YiyiF <yiyifu97@gmail.com>
2020-12-22 23:09:12 +0800

    Modify bs4.select()

--- luck.py
+++ luck.py
@@ -8,15 +8,15 @@ res = requests.get(url='https://google.com/search?q=' + ' '.join(sys.argv[1:]), 
 try:
     res.raise_for_status()
     # Retrieve top search result links.
-    resultSoup = bs4.BeautifulSoup(res.text)
-
+    resultSoup = bs4.BeautifulSoup(res.text, features='html.parser')
     # Open a browser tab for each result.
     linkElems = resultSoup.select('.yuRUbf a')
     numOpen = min(5, len(linkElems))
     for i in range(numOpen):
         try:
-            webbrowser.open('http://google.com' + linkElems[i].get('href'))
+            print('=== Search results: %s' % str(linkElems[i].get('href')))
+            webbrowser.open(linkElems[i].get('ping'))
         except Exception as whichUrlError:
-            print('There was a problem: %s' % whichUrlError)
+            print('=== The website %s had a problem.' % whichUrlError)
 except Exception as exc:
-    print('There was a problem: %s' % exc)
+    print('=== There was a problem: %s' % exc)
